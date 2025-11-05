159e83f9f05e3246be42cf62d1422e09c70a5b98
foldi.soma.vajk <ugylokuka@gmail.com>
2025-11-05 08:26:29 +0100

    asdasd

--- Euroskills2018/Program.cs
+++ Euroskills2018/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.Sqlite;
+using Euroskills2018.Data;
 
 namespace Euroskills2018
 {
@@ -8,11 +11,16 @@ namespace Euroskills2018
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<EuroskillsContext>(options =>
+            {
+                options.UseSqlite("Data Source=euroskills2018.db");
+            });
 
             var app = builder.Build();
 
@@ -27,8 +35,38 @@ namespace Euroskills2018
 
             app.UseAuthorization();
 
-
             app.MapControllers();
+
+            // Adatbázis inicializálás és SQL szkriptek futtatása
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var db = services.GetRequiredService<EuroskillsContext>();
+                db.Database.EnsureCreated();
+
+                var dbFile = Path.Combine(Directory.GetCurrentDirectory(), "euroskills2018.db");
+                if (File.Exists(dbFile))
+                {
+                    using var conn = new SqliteConnection($"Data Source={dbFile}");
+                    conn.Open();
+                    using var cmd = conn.CreateCommand();
+                    var sqlFolder = Path.Combine(Directory.GetCurrentDirectory(), "Sql");
+                    var schemaPath = Path.Combine(sqlFolder, "tablak.sql");
+                    var dataPath = Path.Combine(sqlFolder, "adatok.sql");
+
+                    if (File.Exists(schemaPath))
+                    {
+                        cmd.CommandText = File.ReadAllText(schemaPath);
+                        try { cmd.ExecuteNonQuery(); } catch { }
+                    }
+
+                    if (File.Exists(dataPath))
+                    {
+                        cmd.CommandText = File.ReadAllText(dataPath);
+                        try { cmd.ExecuteNonQuery(); } catch { }
+                    }
+                }
+            }
 
             app.Run();
         }
